6c78fb5da81bf339359f07f24a3339057be48094
Dries Vints <dries.vints@gmail.com>
2017-02-18 16:59:22 +0100

    Remove closures from route files

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,4 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Removed routes now to avoid closures.
