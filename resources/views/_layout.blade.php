68e3cbec4266071cb2db7808343dd4d05e349932
Dries Vints <dries.vints@gmail.com>
2018-08-25 18:52:48 +0200

    Use noindex meta tag instead of robots.txt

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    @unless (request()->is('/'))
+        {{-- We don't want to allow indexing of pastes. --}}
+        <meta name="robots" content="noindex">
+    @endif
+
     <title>Pastebin | Laravel.io</title>
 
     <link rel="stylesheet" href="{{ asset('legacy/app.css') }}">
