a22cc8a37aec2857e40fbdc46e0db782cdf4feb2
Dries Vints <dries.vints@gmail.com>
2018-09-18 16:31:12 +0200

    Add layout to raw pastes

--- resources/views/raw.blade.php
+++ resources/views/raw.blade.php
@@ -1,1 +1,21 @@
-<pre>{{ $paste->code }}</pre>
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @unless (request()->is('/'))
+        {{-- We don't want to allow indexing of pastes. --}}
+        <meta name="robots" content="noindex">
+    @endif
+
+    <title>Pastebin | Laravel.io</title>
+
+    @include('_google_analytics')
+    @include('_favicons')
+</head>
+<body>
+    <pre>{{ $paste->code }}</pre>
+</body>
+</html>
