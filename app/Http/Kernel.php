525fc5ef8d417db704ce672201a693acc84e3818
Dries Vints <dries.vints@gmail.com>
2018-10-21 15:04:37 +0000

    Apply fixes from StyleCI

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,7 +59,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];    
+    ];
 
     /**
      * The priority-sorted list of middleware.
