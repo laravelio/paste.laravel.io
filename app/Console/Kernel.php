ca59bbb5faf34e49ef21e06ad43a6d79277f8788
Dries Vints <dries@vints.io>
2020-11-22 21:46:20 +0100

    Remove spatie backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('02:00');
+        //
     }
 
     /**
