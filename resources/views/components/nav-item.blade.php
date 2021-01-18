22c55230ad02ddbde2d1a7b1276842e7828e01d7
Enzo Innocenzi <enzo@innocenzi.dev>
2021-01-18 21:13:14 +0100

    fix: make navigation item styles consistent

--- resources/views/components/nav-item.blade.php
+++ resources/views/components/nav-item.blade.php
@@ -9,9 +9,10 @@
         {{ $label }}
     </a>
 @else
-    <div class="inline-flex items-center w-full px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 dark:text-white bg-gray-200 dark:bg-gray-500 hover:bg-gray-300 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+    <label {{ $attributes->merge(['class' => 'inline-flex items-center w-full px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-gray-700 dark:text-white bg-gray-200 dark:bg-gray-500 hover:bg-gray-300 dark:hover:bg-gray-600 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer']) }} >
         @svg($icon, '-ml-0.5 mr-2 h-4 w-4')
 
-        <input type="{{ $type }}" value="{{ $label }}" {{ $attributes->merge(['class' => 'bg-transparent cursor-pointer font-medium']) }} />
-    </div>
+        <span class="sr-only">{{ $label }}</span>
+        <input type="{{ $type }}" value="{{ $label }}" class="bg-transparent focus:outline-none cursor-pointer" />
+    </label>
 @endisset
