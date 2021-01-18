a5cf64b04c27e52f811b026db6acafd7b210bc4c
Dries Vints <dries@vints.io>
2021-01-18 20:11:45 +0100

    Fix editor bg

--- resources/views/_editor.blade.php
+++ resources/views/_editor.blade.php
@@ -1,3 +1,5 @@
 <div class="h-full font-mono text-sm">
-    <textarea name="code" class="w-full h-full resize-none outline-none">{{ optional($paste ?? null)->code }}</textarea>
+    <textarea
+        name="code" class="dark:bg-dark text-gray-900 dark:text-white w-full h-full resize-none outline-none"
+    >{{ optional($paste ?? null)->code }}</textarea>
 </div>
