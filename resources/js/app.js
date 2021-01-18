ec8926b804fc45ea8bb7b4048ea88a5fcef914dd
Dries Vints <dries@vints.io>
2021-01-18 15:47:58 +0100

    Textarea indentation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,20 @@
 require('./bootstrap');
 
 import 'alpinejs';
+
+// Textarea identation support
+import * as indentation from 'indent-textarea';
+
+const textarea = document.querySelector('textarea');
+indentation.watch(textarea);
+
+// Highlight code blocks and line numbers
 const hljs = require('highlight.js');
 
 window.hljs = hljs;
 
 require('highlightjs-line-numbers.js');
 
-// Highlight code blocks and line numbers
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('pre code').forEach((block) => {
         hljs.highlightBlock(block);
