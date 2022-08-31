d787f8bbea87a35f93b9c18f17958a579bdcf627
thinkverse <hallberg.kim@gmail.com>
2022-08-31 20:05:36 +0200

    Fix unable to resolve asset

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [laravel(['resources/css/app.css', 'resources/js/app.js'])],
+    plugins: [laravel(['resources/css/app.scss', 'resources/js/app.js'])],
 });
