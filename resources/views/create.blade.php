bee8b028af2725fec94d119b4a49c027556a2b49
thinkverse <hallberg.kim@gmail.com>
2022-09-09 09:37:30 +0200

    Update save icon `arrow-down-on-square` -> `folder-plus`

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -10,7 +10,7 @@
             </x-main>
 
             <x-nav>
-                <x-nav-item label="Save" type="submit" icon="heroicon-o-arrow-down-on-square" />
+                <x-nav-item label="Save" type="submit" icon="heroicon-o-folder-plus" />
                 <x-nav-item label="Reset" type="reset" icon="heroicon-o-no-symbol" />
             </x-nav>
         </div>
