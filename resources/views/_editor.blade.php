bd81963914b1692ec7fb10d74710c8866d716f43
Mohamed Amine EL ATTABI <mohamed.amine.elattabi@gmail.com>
2022-09-14 15:11:39 +0100

    Add placeholder to editor

--- resources/views/_editor.blade.php
+++ resources/views/_editor.blade.php
@@ -1,5 +1,6 @@
 <div class="h-full font-mono text-sm">
     <textarea
         name="code" class="dark:bg-dark text-gray-900 dark:text-white w-full h-full resize-none outline-none"
+        placeholder="Share your snippet here!"
     >{{ optional($paste ?? null)->code }}</textarea>
 </div>
