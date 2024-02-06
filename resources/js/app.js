30d4ec0db87e671df67a314717a6bd3d5673a27c
Dries Vints <dries@vints.be>
2024-02-06 16:17:07 +0100

    Update Alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import './bootstrap';
 
-import 'alpinejs';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
 
 // Textarea identation support
 import * as indentation from 'indent-textarea';
