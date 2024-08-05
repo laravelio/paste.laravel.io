da9deb1ac06a3a2eb188f49e4aac165a70ab9987
Alpha Olomi <alphaolomi@gmail.com>
2024-08-05 07:03:00 +0300

    allow test withoutVite manifest + update lock

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
